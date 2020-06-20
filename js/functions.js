83165954e814ceaa8cd5c9603bf79d2e63e57541
Anna Książek <anna.ksiazek32@gmail.com>
2020-06-20 17:08:54 +0200

    Add computer moves

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2) {
+    computerMove = 'papier'
+} else if(randomNumber == 3) {
+    computerMove = 'nożyce'
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+printMessage('Mój ruch to: ' + computerMove);
