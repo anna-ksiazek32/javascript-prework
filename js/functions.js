d7425d39022da5e37f051b3f2a5412b4ae563e6b
Anna Książek <anna.ksiazek32@gmail.com>
2020-06-20 15:54:43 +0200

    Add commit

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+let computerMove = `kamień`;
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
